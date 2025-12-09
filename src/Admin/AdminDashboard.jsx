d72faf183cfb1b968a3c0fa20cdd73d164b8f4c5
Infantjebasurya <infantjebasurya@gmail.com>
2025-12-09 21:56:44 +0530

    fixied-relode

--- src/Admin/AdminDashboard.jsx
+++ src/Admin/AdminDashboard.jsx
@@ -1,25 +1,32 @@
 // src/Admin/AdminDashboard.jsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
 import DashboardOverview from './components/Dashboard/DashboardOverview';
 import Users from './components/Users/Users';
 import Organizations from './components/Organizations/Organizations';
-import Feedbacks from './components/Feedbacks/Feedbacks'; // Add this import
+import Feedbacks from './components/Feedbacks/Feedbacks';
 import AdminPlanUpgrade from './components/PlanUpgrade/AdminPlanUpgrade';
 
 const AdminDashboard = ({ darkMode }) => {
+  const location = useLocation();
+  
+  // Log for debugging
+  console.log('AdminDashboard location:', location.pathname);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Routes>
+        <Route index element={<DashboardOverview darkMode={darkMode} />} />
         <Route path="/" element={<DashboardOverview darkMode={darkMode} />} />
         <Route path="/users" element={<Users darkMode={darkMode} />} />
         <Route path="/users/*" element={<Users darkMode={darkMode} />} />
         <Route path="/organizations" element={<Organizations darkMode={darkMode} />} />
-        <Route path="/AdminPlan" element={<AdminPlanUpgrade darkMode={darkMode} />} />
-
-        {/* Add the Feedbacks route */}
+        <Route path="/organizations/*" element={<Organizations darkMode={darkMode} />} />
+        <Route path="/adminplan" element={<AdminPlanUpgrade darkMode={darkMode} />} />
+        <Route path="/adminplan/*" element={<AdminPlanUpgrade darkMode={darkMode} />} />
         <Route path="/feedbacks" element={<Feedbacks darkMode={darkMode} />} />
+        <Route path="/feedbacks/*" element={<Feedbacks darkMode={darkMode} />} />
         {/* Add other admin routes here */}
         <Route path="*" element={<DashboardOverview darkMode={darkMode} />} />
       </Routes>
